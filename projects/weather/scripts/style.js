f26a0d04ba6e35230f02df50c3c3e91772d12a58
BradleyGilden <bradleygilden@gmail.com>
2023-10-08 14:21:55 +0200

    styles update

---
+++ projects/weather/scripts/style.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+/**
+ * helper module for website layout
+ * Author: Bradley Dillion Gilden
+ * Date: 07-10-2023
+ */
+
+window.addEventListener('load', () => {
+  let links = document.querySelectorAll('.selector');
+  for (let link of links) {
+    if (link.href === window.location.href) {
+      link.classList.add('visited');
+    } else {
+      link.classList.remove('visited');
+    }
+  }
+})
