177664c05e712647b7cb466f98df8473be845527
BradleyGilden <bradleygilden@gmail.com>
2023-10-11 19:44:11 +0200

    styles added

--- projects/weather/scripts/style.js
+++ projects/weather/scripts/style.js
@@ -7,12 +7,17 @@
  */
 
 window.addEventListener('load', () => {
+  let def = true;
   let links = document.querySelectorAll('.selector');
   for (let link of links) {
     if (link.href === window.location.href) {
       link.classList.add('visited');
+      def = false
     } else {
       link.classList.remove('visited');
     }
+  }
+  if (def) {
+    links[0].classList.add('visited');
   }
 })
