c6780ed25a55f3eb38dd05f447c090f57a86f0f9
Bradley Gilden <bradleygilden@gmail.com>
2023-12-18 20:35:03 +0000

    dynamic list generation

--- React/tutorial/src/index.js
+++ React/tutorial/src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// importing image as component
+import bookCover1 from './images/bookcover1.jpg';
+import bookCover2 from './images/bookcover2.jpg';
+import bookCover3 from './images/bookcover3.jpg';
+import bookCover4 from './images/bookcover4.jpg';
 import './index.css';
 
-const Image = () => <img className="pt-4 w-10/12 mx-auto" src="https://images-na.ssl-images-amazon.com/images/W/MEDIAX_792452-T1/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg" alt="cover" />;
-const Title = () => <h2 className="text-center px-2 sm:text-lg sm:px-4" >Interesting Facts For Curious Minds</h2>;
-const Author = () => <h4 className="text-center tracking-wide">Jordan Moore</h4>;
+const book1 = {
+  title: 'Curious Minds',
+  author: 'Jordan Moor',
+  img: bookCover1
+}
 
-const Book = () => {
+const book2 = {
+  title: 'Holly',
+  author: 'Stephen King',
+  img: bookCover2
+}
+
+const book3 = {
+  title: 'Dad Jokes',
+  author: 'Jimmy Niro',
+  img: bookCover3
+}
+
+const book4 = {
+  title: 'Fourth Wing',
+  author: 'Rebecca Yarros',
+  img: bookCover4
+}
+
+// props
+const Book = (props) => {
   return (
     <article className="sm:w-80 w-11/12 bg-white rounded-[30px]">
-      <Image />
-      <Title />
-      <Author />
+      <img className="pt-4 w-10/12 mx-auto" src={props.img} alt="cover" />
+      <h2 className="text-center px-2 sm:text-lg sm:px-4" >{props.title}</h2>
+      {/* we can add some javascript functionality in braces, only expressions are evaluated */}
+      <h4 className="text-center text-sm tracking-wider">{props.author.toUpperCase()}</h4>
     </article>
   );
 };
@@ -20,10 +47,10 @@ function BookList() {
   return (
     <div className="w-full">
       <section className="flex flex-wrap gap-y-6 sm:gap-4 justify-center py-12 px-0 sm:w-10/12 mx-auto">
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book img={book1.img} title={book1.title} author={book1.author} />
+        <Book img={book2.img} title={book2.title} author={book2.author} />
+        <Book img={book3.img} title={book3.title} author={book3.author} />
+        <Book img={book4.img} title={book4.title} author={book4.author} />
       </section>
     </div>
   );
