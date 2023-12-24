5f51710ab7aea0153658a8d8185038d1319e2da5
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 16:47:17 +0200

    user login challenge

--- React/advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ React/advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -17,8 +17,7 @@ const UserChallenge = () => {
         <h2>Please Login</h2>
         <button type='button' className='btn'  style={{ margin: '1rem 0' }} onClick={handleLogin} >login</button>
       </>
-      )
-    : (
+      ) : (
       <>
         <h2>Hello there, {user.name}</h2>
         <button type='button' className='btn'  style={{ margin: '1rem 0' }} onClick={handleLogin}>logout</button>
