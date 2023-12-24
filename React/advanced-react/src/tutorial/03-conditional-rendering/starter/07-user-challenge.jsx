72138ee84fde93c7b7161f1af192aa1d4a3bfbeb
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 14:39:31 +0000

    Sun, Dec 24, 2023, 4:39 PM +02:00

--- React/advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ React/advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -7,8 +7,10 @@ const UserChallenge = () => {
   return (
   <div>
     { user ? (
-      <h2>Please Login</h2>
-      <button type='button' className='button'  style={{ margin: '1rem 0' }}>login</button>
+      <>
+        <h2>Please Login</h2>
+        <button type='button' className='button'  style={{ margin: '1rem 0' }}>login</button>
+      </>
       )
     : ('Hello there, ' + user.name) }
   </div>
