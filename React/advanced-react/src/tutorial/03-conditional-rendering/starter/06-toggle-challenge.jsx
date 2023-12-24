50ffeee15d87d0899769a94c7cca16b8f1af63d7
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 13:54:34 +0000

    Sun, Dec 24, 2023, 3:54 PM +02:00

--- React/advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ React/advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,18 +1,34 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const ToggleChallenge = () => {
 
   const [name, setName] = useState('john');
   const [morning, setMorning] = useState(true);
+  const [message, setMessage] = useState('change to night');
+
+  const toggleButton = () => {
+    if (morning) {
+      setMorning(true);
+      setMessage('change to night');
+    } else {
+      setMorning(false);
+      setMessage('change to morning');
+    }
+  }
 
   return (
-    {name && <Greeting status={morning} name={name} />}
+    <>
+      {name && <Greeting status={morning} name={name} message={message} />}
+    </>
   );
 };
 
 const Greeting = ({status, name}) => {
   return (
-    {status ? (<h2>Good Morning {name}</h2>) : (<h2>Good Night {name}</h2>)}
+    <>
+      { status ? (<h2>Good Morning {name}</h2>) : (<h2>Good Night {name}</h2>) }
+      <button type='button' className='btn' style={{ margin: '1rem' }}>{ message }</button>
+    </>
   );
 }
 
