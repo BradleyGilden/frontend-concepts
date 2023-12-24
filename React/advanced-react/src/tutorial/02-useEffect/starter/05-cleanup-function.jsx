0bdd3fcab952dc1f8497bf061d2c6a717c5a47ec
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 17:00:01 +0000

    Sun, Dec 24, 2023, 7:00 PM +02:00

--- React/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ React/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -2,7 +2,7 @@ const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
   return (
   <div>
-    <button className='btn' onClick={() => setToggle(!toggle)}>
+    <button type='button' className='btn' onClick={() => setToggle(!toggle)}>
       toggle component
     </button>
   </div>
