c93588cff160deae4674a8dd6d74426e823af0dd
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 13:15:10 +0000

    Sun, Dec 24, 2023, 3:15 PM +02:00

--- React/advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ React/advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  const [emptyStr, setEmptyStr] = useState('');
+  const [zero, setZero] = useState(0);
   const [nonZero, setNonZero] = useState(10);
 
-  
+
   return <h2>short circuit overview</h2>;
 };
 export default ShortCircuitOverview;
