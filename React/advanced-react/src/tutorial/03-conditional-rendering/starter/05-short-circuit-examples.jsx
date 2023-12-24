4328d1718bca7496e53a38e63568f29f70593032
Bradley Gilden <bradleygilden@gmail.com>
2023-12-24 13:33:06 +0000

    Sun, Dec 24, 2023, 3:33 PM +02:00

--- React/advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ React/advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -8,12 +8,20 @@ const ShortCircuitExamples = () => {
   const [user, setUser] = useState({ name: 'john' });
   const [isEditing, setIsEditing] = useState(false);
 
+  const toggleBtn = () => {
+    if (isEditing) {
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    }
+  };
+
   return (
     <div>
       <h2>{text || 'default value'}</h2>
       {user && <SomeComponent name={user.name} />}
       <h2 style={{ margin: '1rem 0' }}>Ternary operator</h2>
-      <button type='button' className='btn' onlClick=>{isEditing ? 'edit':'add'}</button>
+      <button type='button' className='btn' onClick={toggleBtn}>{isEditing ? 'edit':'add'}</button>
     </div>
   );
 };
