6481e3f7b30ec50638fd329e592df01586f2da46
Bradley Gilden <bradleygilden@gmail.com>
2023-12-25 08:17:19 +0000

    Mon, Dec 25, 2023, 10:17 AM +02:00

--- React/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ React/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 
 const CleanupFunction = () => {
   const [toggle, setToggle] = useState(false);
+  console.log('render');
   return (
   <div>
     <button type='button' className='btn' onClick={() => setToggle(!toggle)}>
@@ -15,12 +16,12 @@ const CleanupFunction = () => {
 
 const RandomComponent = () => {
   useEffect(() => {
-    console.log('hmm, this is interesting');
     const intId = setInterval(() => {
       console.log('hello from interval');
     }, 1000);
     return () => {
       clearInterval(intId);
+      console.log('cleanup');
     };
   }, []);
   return <h1>hello there</h1>
