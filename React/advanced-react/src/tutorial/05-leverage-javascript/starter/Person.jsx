becfa6634ae04995455e6e8b8924d7ebdb24ab3c
Bradley Gilden <bradleygilden@gmail.com>
2023-12-26 21:38:22 +0000

    Tue, Dec 26, 2023, 11:38 PM +02:00

--- React/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ React/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -3,10 +3,10 @@ import avatar from '../../../assets/default-avatar.svg';
 const Person = ({person}) => {
 	return (
 		<div key={person.id} className='item'>
-			<h4>{person.name}</h4>
+			<h4 style={{  marginBottom: '0px' }}>{person.name}</h4>
 			{/* conditional returns */}
-			<p>{ person.nickName && ('Nickname: ' + person?.nickName ?? '')}</p>
-			<img alt={ name } style={{ width: '60px', marginBottom: '0px', marginTop: '1rem' }} src={person?.images?.[0]?.small?.url ?? avatar }></img>
+			<p style={{  marginBottom: '1rem' }}>{ person.nickName && ('Nickname: ' + person?.nickName ?? '')}</p>
+			<img alt={ name } style={{ width: '60px', marginBottom: '2rem' }} src={person?.images?.[0]?.small?.url ?? avatar }></img>
 		</div>
 	);
 }
