2ce4fffe95f038dc6dbda2358348bc0f549d62ad
Bradley Gilden <bradleygilden@gmail.com>
2023-12-26 20:49:53 +0000

    Tue, Dec 26, 2023, 10:49 PM +02:00

--- React/advanced-react/src/tutorial/05-leverage-javascript/starter/List.jsx
+++ React/advanced-react/src/tutorial/05-leverage-javascript/starter/List.jsx
@@ -1,7 +1,13 @@
 import { people } from '../../../data';
 
 const List = () => {
-
+  return (
+      people.map((person) => (
+        <div key={person.id} className='item'>
+          <p>{person.name}</p>
+        </div>
+      ))
+  );
 }
 
 export default List;
