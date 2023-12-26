8d1dd073d42264a01b999032860df9adb29c2786
Bradley Gilden <bradleygilden@gmail.com>
2023-12-26 21:09:37 +0000

    Tue, Dec 26, 2023, 11:09 PM +02:00

--- React/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ React/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -3,7 +3,7 @@ const Person = ({person}) => {
 		<div key={person.id} className='item'>
 			<h4>{person.name}</h4>
 			<p>{person?.nickName ?? ''}</p>
-			<img src={person?.images?.small?.url ?? ''}></img>
+			<img style={{ width: '60px' }} src={person?.images?.[0]?.small?.url ?? ''}></img>
 		</div>
 	);
 }
